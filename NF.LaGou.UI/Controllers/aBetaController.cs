10fce11fa1c5a31a32f752e527ebf127381ab310
wingedgirl <wingedgirl@126.com>
2017-04-20 10:08:35 +0800

    一拍页面

---
+++ NF.LaGou.UI/Controllers/aBetaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NF.LaGou.UI.Controllers
+{
+    public class aBetaController : Controller
+    {
+        //
+        // GET: /aBeta/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
